c90b3e9040d1e317d14da7d443033a3452fd8651
onassis <onassis.dev@gmail.com>
2024-10-24 10:58:36 -0700

    added csx

--- src/components/Websites.tsx
+++ src/components/Websites.tsx
@@ -7,8 +7,11 @@ const Websites = () => {
             <Website title="IglesiasBC" href="https://iglesiasbc.com" img="/1.png" value="11" liked={true}>
                 Un SaaS que desarrollé completamente por mi cuenta. Este busca ser una solución todo en uno para las iglesias, permitiendo administrar miembros, finanzas, clases, inventario y crear certificados, página web y blog. <br /> También permite colaborar con otros usuarios para llevar la administración entre varias personas, así logrando unificarlo todo en una plataforma.
             </Website>
-            <Website title="Sistema Interno" href="" img="/2.png" value="12" liked={true}>
+            <Website title="Sistema Interno" href="" img="/2.png" value="14" liked={true}>
                 Un software ERP personalizado. Este cuenta con módulo de inventario, recursos humanos y archivos, y tiene varias funciones personalizadas para la empresa que permiten ahorrar mucho trabajo al momento de registrar o leer información, tales como diversos gráficos, intérpretes de archivos PDF personalizados, importación y exportación de datos, validación de campos, entre otros.
+            </Website>
+            <Website title="CSX" href="https://csx.one" img="/7.png" value="12" liked={true}>
+                Un ecommerce completo de ropa deportiva con un constructor para personalizar las prendas a tu medida, que te permite crear tus propias prendas a tu estilo fácilmente.
             </Website>
             <Website title="Portafolio fotográfico elegante" href="https://photographer1.onassis.dev/" img="/3.png" value="3" liked={false}>
                 Una galería de fotos moderna y elegante.
