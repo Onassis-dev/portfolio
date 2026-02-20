7ce5f9f60e01a43f3f5a08be55c13830e33f3639
Onassis <onassis.dev@gmail.com>
2026-02-19 19:01:18 -0800

    feat: configure trailing slash behavior in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
+  trailingSlash: "never",
   output: "static",
   adapter: cloudflare(),
 });
